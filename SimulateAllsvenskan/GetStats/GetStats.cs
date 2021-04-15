414665b1960c6fc8026feba2a43a4266416bd88c
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2021-04-15 22:46:28 +0200

    Getting team stats from season
    Added a program to get average goals scored and admitted from every team in a season.

---
+++ SimulateAllsvenskan/GetStats/GetStats.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SimulateAllsvenskan.Common;
+using System.Xml.Serialization;
+
+namespace SimulateAllsvenskan.GetStats
+{
+    class GetStats
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("Input.txt");
+
+            List<Team> teams = new List<Team>();
+
+            string inputRow;
+            while ((inputRow = reader.ReadLine()) != null)
+            {
+                if (inputRow != "" && inputRow[0] != '#') //Filters out empty rows ans comments
+                {
+                    string[] teamInfo = inputRow.Split('\t');
+
+                    Team team = new Team();
+                    team.Name = teamInfo[1];
+
+                    double scored = double.Parse(teamInfo[6]);
+                    double admitted = double.Parse(teamInfo[7]);
+                    int gamesPlayed = int.Parse(teamInfo[2]);
+
+                    team.AvrgScored = scored / gamesPlayed;
+                    team.AvrgAdmitted = admitted / gamesPlayed;
+
+                    teams.Add(team);
+                }
+            }
+            reader.Close();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Team>));
+            StreamWriter writer = new StreamWriter("Teams.xml", false);
+            serializer.Serialize(writer, teams);
+            writer.Close();
+
+            Console.WriteLine("Analyzed {0} teams and wrote to output: ", teams.Count);
+            foreach (Team team in teams)
+                Console.WriteLine(team.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimulateAllsvenskan/Teams/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateAllsvenskan.Common
+{
+    public class Team
+    {
+        private string _name;
+        private double _avrgScored, _avrgAdmitted;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public double AvrgScored { get { return _avrgScored; } set { _avrgScored = value; } }
+        public double AvrgAdmitted { get { return _avrgAdmitted; } set { _avrgAdmitted = value; } }
+
+        public Team()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Name + "\tavgScore: " + AvrgScored + "\tavgAdmitted: " + AvrgAdmitted;
+        }
+    }
+}
